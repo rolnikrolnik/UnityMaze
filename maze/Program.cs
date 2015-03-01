ec5c5c81369ce06187db6e55565c6b1e611ba69c
Kacper <kdebowski93@gmail.com>
2015-03-01 13:19:33 +0100

    Maze full of walls

--- maze/Program.cs
+++ maze/Program.cs
@@ -10,6 +10,18 @@ namespace maze
     {
         static void Main(string[] args)
         {
+            char[][] maze = new char[10][];
+            for (int i = 0; i < 10; i++)
+			{
+			    maze[i] = new char[10];
+			}
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    maze[i][j] = '#';
+                }
+            }
         }
     }
 }
