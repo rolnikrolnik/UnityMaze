c722a44b6c60aa928ebdba45f05a3ca1eee6c1a4
kubajvj <jakub.bielawa@gmail.com>
2015-08-12 20:59:03 +0200

    Movement test implementation

--- TreasureHunter/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ TreasureHunter/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -13,7 +13,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
-        
+		private float rotationSpeed = 100.0f;
+		private float walkSpeed = 20.0f;
+
         private void Start()
         {
             // get the transform of the main camera
@@ -45,30 +47,30 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         // Fixed update is called in sync with physics
         private void FixedUpdate()
         {
-            // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+			// read inputs
+			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			float v = CrossPlatformInputManager.GetAxis ("Vertical");
+			bool crouch = Input.GetKey (KeyCode.C);
 
-            // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
-                // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
-            }
-            else
-            {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
-            }
-#if !MOBILE_INPUT
-			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
-#endif
+			var translation = v * walkSpeed;
+			var rotation = h * rotationSpeed;
+
+			translation *= Time.deltaTime;
+			rotation *= Time.deltaTime;
+
+	        if (Input.GetKey (KeyCode.LeftShift)) {
+				translation *= 2.0f;
+			}
+
+			transform.Translate (0, 0, translation);
+			transform.Rotate (0, rotation, 0);
+
+			var h_mouse = rotationSpeed * Input.GetAxis ("Mouse X");
+			//var v_mouse = -verticalSpeed * Input.GetAxis ("Mouse Y");
+			transform.Rotate (/*v_mouse*/0, h_mouse * 0.25f, 0);
 
             // pass all parameters to the character control script
-            m_Character.Move(m_Move, crouch, m_Jump);
+			m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
         }
     }
