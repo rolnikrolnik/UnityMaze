f8a2a310cbd5932e7eb4afcdc97915c3a7997a61
Dominik <dominikpielak@gmail.com>
2015-11-16 23:38:06 +0100

    FinalRoom added to MazeGeneratorTestScene

---
+++ TreasureHunter/Assets/Scripts/Controllers/Gameplay/FinalRoomController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Treasure_Hunter.Controllers
+{
+    public class FinalRoomController : MonoBehaviour
+    {
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
---
+++ TreasureHunter/Assets/Scripts/Controllers/Gameplay/StaticRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Treasure_Hunter.Controllers
+{
+    public class StaticRotation : MonoBehaviour
+    {
+
+        public Vector3 Rotation;
+
+        private void Update()
+        {
+            transform.Rotate(Rotation);
+        }
+    }
+}
