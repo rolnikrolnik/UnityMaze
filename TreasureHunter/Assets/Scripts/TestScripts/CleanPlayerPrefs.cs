38f08ae80a80369b04bf08fda1980ca507f8d72a
Dominik <dominikpielak@gmail.com>
2016-01-05 21:54:45 +0100

    CleanPlayerPrefs

---
+++ TreasureHunter/Assets/Scripts/TestScripts/CleanPlayerPrefs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CleanPlayerPrefs : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
